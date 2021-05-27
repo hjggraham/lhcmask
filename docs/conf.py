be6a520d4531a46960592ef24b0de88c23c8d846
hjggraham <hamish@graham.gb.net>
2021-05-27 11:39:07 +0200

    update

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../pymask'))
+sys.path.insert(0, os.path.abspath('../pymask'))
 
 
 # -- Project information -----------------------------------------------------
